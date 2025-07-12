ddd829f14ac9899dec3dda4d18d1d9f069df8b50
MatDerek <mattamab@mtechlabs.dev>
2025-07-12 13:18:24 +0800

    app service

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,25 +1,17 @@
-import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ClientKafka } from '@nestjs/microservices';
+
 
 @Controller()
 export class AppController {
-  private logger = new Logger(AppController.name)
   constructor(
     private readonly appService: AppService,
-    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka
+    
   ) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
-
-  @Post('order')
-  handleOrderCreated(@Body() order: any) {
-    this.kafkaClient.emit('order.created', order);
-    
-    this.logger.debug("Order Created",order);
-    return order;
+  @Post('orders')
+  kafkaEntry(@Body() order: any) {
+    this.appService.handleOrder(order)
+    return {success: true , order};
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  private logger = new Logger(AppService.name);
+  constructor(@Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka) {}
+
+  handleOrder(order: any) {
+    this.kafkaClient.emit('order.created', order);
+    this.logger.debug("Order Created",order);
   }
 }
